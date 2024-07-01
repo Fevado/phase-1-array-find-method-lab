5f8bef23022311636fb1f6177f18274cbe70433a
favoured mwange <mwangefavoured@gmail.com>
2024-07-01 13:17:31 +0300

    adds array.find

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    let wRecord= 
+    record.find(record=>record.result==="W");
+    return wRecord?
+    wRecord.year : undefined;
+
+}
+
